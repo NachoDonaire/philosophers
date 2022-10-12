e27f8d96ff9a09ed4bd9186599e1a8b5e450cdaf
NachoDonaire <nacho23donaire@gmail.com>
2022-10-12 23:40:49 +0200

    [start] pruebas

--- includes/philosophers.h
+++ includes/philosophers.h
@@ -7,11 +7,24 @@
 
 typedef struct philos_data{
 	int	state;
+	int	r_fork;
+	int	l_fork;
+	int	is_dead;
+	pthread_t	pthread;
 }	t_philos_data;
 
 typedef struct	general_data{
 	pthread_t	*n_philos;
 	t_philos_data	*philos;
+	int		n_philo;
+	int		t_die;
+	int		t_eat;
+	int		t_sleep;
+	int		n_philos_dead;
+	int		pid;
+	char		**args;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	util;
 }	t_gen_data;
 
 int	ft_atoi(char *s);
--- src/philosophers.c
+++ src/philosophers.c
@@ -1,16 +1,18 @@
 #include "../includes/philosophers.h"
 
-/*
 void	*routineA()
 {
 	int 	i;
+	int	a;
 
+	a = 0;
 	i = 0;
+	
 	while (i++ < 2000)
 		a++;
 	return (0);
 }
-
+/*
 void	*routineB()
 {
 	int	i;
@@ -24,20 +26,53 @@ void	*routineB()
 	return (0);
 }
 */
-int		gemiro = 0;
+
+void	fill_forks(t_gen_data *gen_data)
+{
+	int	i;
+	int	y;
+
+	y = 0;
+	i = 0;
+	while (y < gen_data->n_philo)
+	{
+		gen_data->philos[y].r_fork = i;
+		if (i == 0)
+			gen_data->philos[y].l_fork = gen_data->n_philo - 1;
+		else
+			gen_data->philos[y].l_fork = i - 1;
+		y++;
+		i++;
+	}
+}
+
 void	preliminar(t_gen_data *gen_data, char **s)
 {
 	int	i;
 
 	i = 0;
-	gen_data->philos = malloc(sizeof(t_philos_data) * (ft_atoi(s[1]) + 1));
-	gen_data->n_philos = malloc(sizeof(int) * (ft_atoi(s[1] + 1)));
-	while (i < ft_atoi(s[i]))
+	gen_data->n_philo = ft_atoi(s[1]);
+	gen_data->n_philos_dead = 0;
+	gen_data->forks = malloc(sizeof(pthread_mutex_t ) * gen_data->n_philo);
+	//gen_data->util = malloc(sizeof(pthread_mutex_t ) * gen_data->n_philo);
+	while (i < gen_data->n_philo)
+		pthread_mutex_init(&gen_data->forks[i++], NULL);
+	i = 0;
+	//while (i < gen_data->n_philo)
+	pthread_mutex_init(&gen_data->util, NULL);
+//	i = 0;
+	gen_data->philos = malloc(sizeof(t_philos_data) * (gen_data->n_philo + 1));
+	gen_data->n_philos = malloc(sizeof(pthread_t *) * (gen_data->n_philo + 1));
+	//pthread_mutex_init(gen_data->forks, NULL);
+	gen_data->pid = 0;
+	while (i < gen_data->n_philo)
 	{
 		gen_data->n_philos[i] = i;
+		gen_data->philos[i].is_dead = 0;
 		i++;
 	}
 	gen_data->n_philos[i] = -1;
+	fill_forks(gen_data);
 }
 
 void	check_arg(int arg)
@@ -49,79 +84,94 @@ void	check_arg(int arg)
 	}
 }
 
-void	*thread_routine_1(void *)
+void	needed_free(t_gen_data *gen_data)
 {
-	int	i;
-
-	i = 0;
-	printf("sumando..\n");
-	while (i < 200)
+	/*while (i < gen_data->n_philo)
 	{
+		if (gen_data->philos[i].r_fork)
+			free(gen_data->philos[i].r_fork);
+		if (gen_data->philos[i].l_fork)
+			free(gen_data->philos[i].l_fork);
 		i++;
-		gemiro++;
+	}*/
+	free(gen_data->philos);
+	if (gen_data->n_philos)
+		free(gen_data->n_philos);
+	if (gen_data->forks)
+		free(gen_data->forks);
+}
+
+void	eat(t_gen_data	*gen_data)
+{
+	//pthread_mutex_lock(&gen_data->forks[gen_data->philos[gen_data->pid].r_fork]);
+	//pthread_mutex_lock(&gen_data->forks[gen_data->philos[gen_data->pid].l_fork]);
+	pthread_mutex_lock(&gen_data->util);
+	printf("El filósofo %d esta jalando\n", gen_data->pid);
+	usleep(2000000);
+	gen_data->philos[gen_data->pid].is_dead = 1;
+	pthread_mutex_unlock(&gen_data->util);
+	//pthread_mutex_unlock(&gen_data->forks[gen_data->philos[gen_data->pid].r_fork]);
+	//pthread_mutex_unlock(&gen_data->forks[gen_data->philos[gen_data->pid].l_fork]);
+}
+
+void	increment_pid(t_gen_data *gen_data)
+{
+	pthread_mutex_lock(&gen_data->util);
+	gen_data->pid++;
+	pthread_mutex_unlock(&gen_data->util);
+}
+
+void	*routine(void	*tra)
+{
+	t_gen_data	*gen_data;
+	
+	gen_data = (t_gen_data *) tra;
+	eat(gen_data);
+	printf("el filosofo %d ha terminado de comer\n", gen_data->pid);
+	gen_data->pid++;
+	/*while (gen_data->philos[gen_data->pid].is_dead == 0)
+	{
+		eat(gen_data);
+		printf("El filósofo %d está pensando(%d)\n", gen_data->pid, gen_data->philos[gen_data->pid].is_dead);
+		//gen_data->philos[gen_data->pid].is_dead = 1;
 	}
+	printf("llegó: %d", gen_data->pid);
+	*/
+	//increment_pid(gen_data);
+	
 	return (0);
 }
 
-void	*thread_routine_2(void *)
-{
-	int	i;
 
-	i = 0;
-	printf("restando..\n");
-	while (i < 200)
-	{
-		gemiro--;
-		i++;
-	}
-	return (0);
-}
 
 int	main(int arg, char **args)
 {
-	int		argy;
-	t_gen_data	gen_data;
+	t_gen_data	*gen_data;
+	int		i;
 
-	gemiro = 0;
+	i = 0;
+	gen_data = malloc(sizeof(t_gen_data) * 1);
 	if (arg != 2)
 		return (0);
-	preliminar(&gen_data, args);
-	if (0 != pthread_create(&gen_data.n_philos[0], NULL, thread_routine_1, &argy))
+	preliminar(gen_data, args);
+	/*while (i < gen_data->n_philo)
 	{
-		printf("no se creo el hilo");
-		return (0);
-	}
-	if (0 != pthread_create(&gen_data.n_philos[1], NULL, thread_routine_2, &argy))
+		printf("r:%d l:%d\n", gen_data->philos[i].r_fork, gen_data->philos[i].l_fork);
+		i++;
+	}*/
+//	printf("%d\n", gen_data->n_philo);
+	while (i < gen_data->n_philo)
 	{
-		printf("no se creo el hilo");
-		return (0);
+		if (0 != pthread_create(&gen_data->philos[i].pthread, NULL, routine, gen_data))
+			printf("no se creo bien el hilo");
+		i++;
+//		gen_data->pid++;
 	}
-
-	pthread_join(gen_data.n_philos[0], NULL);
-	pthread_join(gen_data.n_philos[1], NULL);
-	printf("--%d--\n", gemiro);
+	i = 0;
+	while (gen_data->pid < gen_data->n_philo)
+	{
+		pthread_join(gen_data->philos[i].pthread, NULL);
+		i++;
+	}
 	return (0);
 }
-
-
-
-
-
-	/*pthread_t	thread_1;
-	pthread_t	thread_2;
-
-	if (0 != pthread_create(&thread_1, NULL, routineA, NULL))
-	{
-		printf("Nose creo el hilo 2");
-		return (0);
-	}
-	if (0 != pthread_create(&thread_2, NULL, routineB, NULL))
-	{
-		printf("Nose creo el hilo 2");
-		return (0);
-	}
-	pthread_join(thread_1, NULL);
-	pthread_join(thread_2, NULL);
-	printf("%d\n", a);
-	return (0);
-	*/
